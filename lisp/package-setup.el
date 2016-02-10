8f5cde82e2ec24fc2d25a6ce86a3d7255375db19
Aidan Dusseault <aidandusseault@gmail.com>
2016-02-10 10:53:14 -0500

    Fixed init bug

--- lisp/package-setup.el
+++ lisp/package-setup.el
@@ -23,9 +23,6 @@
 (ac-config-default)
 (add-to-list 'load-path "~/.emacs.d/ac-dict")
 
-;;Blank-mode
-(add-hook 'prog-mode-hook #'blank-mode)
-
 ;;Emmet-mode
 (add-hook 'sgml-mode-hook #'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  #'emmet-mode) ;; enable Emmet's css abbreviation.
